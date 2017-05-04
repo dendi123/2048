b940c3552a38a1bfe494ea89e81a95863040e691
dendi123 <archangelmaycry@gmail.com>
2017-05-04 13:29:04 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,163 +1,169 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
-#include <cstdlib>
-#include <conio.h>
 #include <Windows.h>
-#include <time.h>
+#include <conio.h>
+#include <iomanip>
 
 using namespace std;
-enum movDir { UP, DOWN, LEFT, RIGHT };
 
+enum MoveDir { UP, DOWN, LEFT, RIGHT };
+void resizeConsole(int width, int height);
+void drawBoard();
+void addTile();
+bool canMove();
+bool testAdd(int x, int y, unsigned int v);
+void waitKey();
+void moveVert(int x, int y, int d);
+void moveHori(int x, int y, int d);
+void move(MoveDir d);
 
-class tile
+int Val[4][4] = { 0 };
+bool blocked[4][4] = { false };
+bool done = false;	
+bool moved = true;
+int score = 0;
+
+int main(int argc, char* argv[])
 {
-public:
-	tile() : val(0), blocked(false) {}
-	unsigned int val;
-	bool blocked;
+	srand(static_cast<unsigned int>(time(NULL)));
+	resizeConsole(800, 800);
+	addTile();
+	while (true)
+	{
+		if (moved!=false) addTile();
+		drawBoard();
+		if (done!=false) break;
+		waitKey();
+	}
+	cout << "Game Over" << endl << endl;
+	return system("pause");
+}
+void resizeConsole(int width, int height)
+{
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, width, height, TRUE);
+}
+void drawBoard()
+{
+	system("cls");
+	cout << "SCORE: " << score << endl << endl;
+	for (int y = 0; y < 4; y++)
+	{
+		cout << "x-------------------------------------------x" << endl << "| ";
+		for (int x = 0; x < 4; x++)
+		{
+			cout << setw(8) << " ";
+			cout << " | ";
+		}
+		cout << endl << "| ";
+		for (int x = 0; x < 4; x++)
+		{
+			if (Val[x][y]==0) cout << setw(8) << " ";
+			else cout << setw(8) << Val[x][y];
+			cout << " | ";
+		}
+		cout << endl << "| ";
+		for (int x = 0; x < 4; x++)
+		{
+			cout << setw(8) << " ";
+			cout << " | ";
+		}
+		cout << endl;
+	}
+	cout << "x-------------------------------------------x" << endl << endl;
 };
-
-class g2048
+void addTile()
 {
-public:
-	g2048() : done(false), win(false), moved(true), score(0) {}
-	void loop()
-	{
-		resizeConsole(800, 800);
-		addTile();
-		while (true)
-		{
-			if (moved) addTile();
-			drawBoard();
-			if (done) break;
-			waitKey();
-		}
-		string s = "Game Over!";
-		if (win) s = "You've made it!";
-		cout << s << endl << endl;
-	}
-private:
-	void drawBoard()
-	{
-		system("cls");
-		cout << "SCORE: " << score << endl << endl;
-		for (int y = 0; y < 4; y++)
-		{
-			cout << "x-------------------------------------------x" << endl << "| ";
-			for (int x = 0; x < 4; x++)
+	for (int y = 0; y < 4; y++)
+		for (int x = 0; x < 4; x++)
+			if (Val[x][y]==0)
 			{
-				cout << setw(8) << " ";
-				cout << " | ";
-			}
-			cout << endl << "| ";
-			for (int x = 0; x < 4; x++)
-			{
-				if (!board[x][y].val) cout << setw(8) << " ";
-				else cout << setw(8) << board[x][y].val;
-				cout << " | ";
-			}
-			cout << endl << "| ";
-			for (int x = 0; x < 4; x++)
-			{
-				cout << setw(8) << " ";
-				cout << " | ";
-			}
-			cout << endl;
-		}
-		cout << "x-------------------------------------------x" << endl << endl;
-	}
-	void waitKey()
-	{
-		moved = false; char c;
-		cout << "Pree WASD not wasd to move "; c = _getch();
-		if (c == 'W' || c == 'w') move(UP);
-		else if (c == 'A' || c == 'a') move(LEFT);
-		else if (c == 'S' || c == 's') move(DOWN);
-		else if (c == 'D' || c == 'd') move(RIGHT);
-		for (int y = 0; y < 4; y++)
-			for (int x = 0; x < 4; x++)
-				board[x][y].blocked = false;
-	}
-	void addTile()
-	{
-		for (int y = 0; y < 4; y++)
-			for (int x = 0; x < 4; x++)
-				if (!board[x][y].val)
+				unsigned int a, b;
+				do
 				{
-					unsigned int a, b;
-					do
-					{
-						a = rand() % 4; b = rand() % 4;
-					} while (board[a][b].val != 0);
+					a = rand() % 4; b = rand() % 4;
+				} while (Val[a][b] != 0);
 
-					int s = rand() % 100;
-					if (s >= 90) board[a][b].val = 4;
-					else board[a][b].val = 2;
-					if (canMove()) return;
-				}
-		done = true;
-	}
-	bool canMove()
-	{
-		for (int y = 0; y < 4; y++)
-			for (int x = 0; x < 4; x++)
-				if (board[x][y].val == 0) return true;
-
-		for (int y = 0; y < 4; y++)
-			for (int x = 0; x < 4; x++)
-			{
-				if (testAdd(x + 1, y, board[x][y].val)) return true;
-				if (testAdd(x - 1, y, board[x][y].val)) return true;
-				if (testAdd(x, y + 1, board[x][y].val)) return true;
-				if (testAdd(x, y - 1, board[x][y].val)) return true;
+				int s = rand() % 100;
+				if (s >= 90) Val[a][b] = 4;
+				else Val[a][b] = 2;
+				if (canMove()) return;
 			}
-		return false;
-	}
-	bool testAdd(int x, int y, unsigned int v)
+	done = true;
+}
+bool canMove()
+{
+	for (int y = 0; y < 4; y++)
+		for (int x = 0; x < 4; x++)
+			if (Val[x][y] == 0) return true;
+
+	for (int y = 0; y < 4; y++)
+		for (int x = 0; x < 4; x++)
+		{
+			if (testAdd(x + 1, y, Val[x][y])) return true;
+			if (testAdd(x - 1, y, Val[x][y])) return true;
+			if (testAdd(x, y + 1, Val[x][y])) return true;
+			if (testAdd(x, y - 1, Val[x][y])) return true;
+		}
+	return false;
+}
+bool testAdd(int x, int y, unsigned int v)
+{
+	if (x < 0 || x > 3 || y < 0 || y > 3) return false;
+	return Val[x][y] == v;
+}
+void waitKey()
+{
+	moved = false; char c;
+	cout << "Pree WASD not wasd to move "; c = _getch();
+	if (c == 'W' || c == 'w') move(UP);
+	else if (c == 'A' || c == 'a') move(LEFT);
+	else if (c == 'S' || c == 's') move(DOWN);
+	else if (c == 'D' || c == 'd') move(RIGHT);
+	for (int y = 0; y < 4; y++)
+		for (int x = 0; x < 4; x++)
+			blocked[x][y] = false;
+}
+void moveVert(int x, int y, int d)
+{
+	if (Val[x][y + d]!=0 && Val[x][y + d] == Val[x][y] && blocked[x][y]==false && blocked[x][y + d]==false)
 	{
-		if (x < 0 || x > 3 || y < 0 || y > 3) return false;
-		return board[x][y].val == v;
+		Val[x][y] = 0;
+		Val[x][y + d] *= 2;
+		score += Val[x][y + d];
+		blocked[x][y + d] = true;
+		moved = true;
 	}
-	void moveVert(int x, int y, int d)
+	else if (Val[x][y + d]==0 && Val[x][y]!=0)
 	{
-		if (board[x][y + d].val && board[x][y + d].val == board[x][y].val && !board[x][y].blocked && !board[x][y + d].blocked)
-		{
-			board[x][y].val = 0;
-			board[x][y + d].val *= 2;
-			score += board[x][y + d].val;
-			board[x][y + d].blocked = true;
-			moved = true;
-		}
-		else if (!board[x][y + d].val && board[x][y].val)
-		{
-			board[x][y + d].val = board[x][y].val;
-			board[x][y].val = 0;
-			moved = true;
-		}
-		if (d > 0) { if (y + d < 3) moveVert(x, y + d, 1); }
-		else { if (y + d > 0) moveVert(x, y + d, -1); }
+		Val[x][y + d] = Val[x][y];
+		Val[x][y] = 0;
+		moved = true;
 	}
-	void moveHori(int x, int y, int d)
+	if (d > 0) { if (y + d < 3) moveVert(x, y + d, 1); }
+	else { if (y + d > 0) moveVert(x, y + d, -1); }
+}
+void moveHori(int x, int y, int d)
+{
+	if (Val[x + d][y]!=0 && Val[x + d][y] == Val[x][y] && blocked[x][y]==false && blocked[x + d][y]==false)
 	{
-		if (board[x + d][y].val && board[x + d][y].val == board[x][y].val && !board[x][y].blocked && !board[x + d][y].blocked)
-		{
-			board[x][y].val = 0;
-			board[x + d][y].val *= 2;
-			score += board[x + d][y].val;
-			board[x + d][y].blocked = true;
-			moved = true;
-		}
-		else if (!board[x + d][y].val && board[x][y].val)
-		{
-			board[x + d][y].val = board[x][y].val;
-			board[x][y].val = 0;
-			moved = true;
-		}
-		if (d > 0) { if (x + d < 3) moveHori(x + d, y, 1); }
-		else { if (x + d > 0) moveHori(x + d, y, -1); }
+		Val[x][y] = 0;
+		Val[x + d][y] *= 2;
+		score += Val[x + d][y];
+		blocked[x + d][y] = true;
+		moved = true;
 	}
-	void move(movDir d)
+	else if (Val[x + d][y]==0 && Val[x][y]!=0)
+	{
+		Val[x + d][y] = Val[x][y];
+		Val[x][y] = 0;
+		moved = true;
+	}
+	if (d > 0) { if (x + d < 3) moveHori(x + d, y, 1); }
+	else { if (x + d > 0) moveHori(x + d, y, -1); }
+}
+void move(MoveDir d)
 	{
 		switch (d)
 		{
@@ -167,7 +173,7 @@ private:
 				int y = 1;
 				while (y < 4)
 				{
-					if (board[x][y].val) moveVert(x, y, -1); y++;
+					if (Val[x][y]!=0) moveVert(x, y, -1); y++;
 				}
 			}
 			break;
@@ -177,7 +183,7 @@ private:
 				int y = 2;
 				while (y >= 0)
 				{
-					if (board[x][y].val) moveVert(x, y, 1); y--;
+					if (Val[x][y]!=0) moveVert(x, y, 1); y--;
 				}
 			}
 			break;
@@ -187,7 +193,7 @@ private:
 				int x = 1;
 				while (x < 4)
 				{
-					if (board[x][y].val) moveHori(x, y, -1); x++;
+					if (Val[x][y]!=0) moveHori(x, y, -1); x++;
 				}
 			}
 			break;
@@ -197,26 +203,8 @@ private:
 				int x = 2;
 				while (x >= 0)
 				{
-					if (board[x][y].val) moveHori(x, y, 1); x--;
+					if (Val[x][y]!=0) moveHori(x, y, 1); x--;
 				}
 			}
 		}
 	}
-	void resizeConsole(int width, int height)
-	{
-		HWND console = GetConsoleWindow();
-		RECT r;
-		GetWindowRect(console, &r);
-		MoveWindow(console, r.left, r.top, width, height, TRUE);
-	}
-	tile board[4][4];
-	bool win, done, moved;
-	unsigned int score;
-};
-
-int main(int argc, char* argv[])
-{
-	srand(static_cast<unsigned int>(time(NULL)));
-	g2048 g; g.loop();
-	return system("pause");
-}
